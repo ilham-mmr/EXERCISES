a8ea9091be3bda949f4a2ed8212e9c3865126147
ilham-mmr <54584081+ilham-mmr@users.noreply.github.com>
2020-04-09 19:03:50 +0800

    Create TestMyFCStaff.java

---
+++ Exercise4/Q3/TestMyFCStaff.java
@@ -0,0 +1,19 @@
+public class TestMyFCStaff {
+    static Scanner console = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the staff name:");
+        String name = console.nextLine();
+        System.out.print("Enter the staff id:");
+        String staffId = console.nextLine();
+        System.out.print("Enter total hours : ");
+        int hoursWork = console.nextInt();
+        System.out.println("Enter total sale: ");
+        double totalSale = console.nextDouble();
+
+        MyFCStaff staff = new MyFCStaff(name,staffId,hoursWork,totalSale);
+        staff.calculateCommission();
+        staff.calculateSalary();
+        staff.displaySalary();
+        }
+   
+© 2020 GitHub, Inc.
